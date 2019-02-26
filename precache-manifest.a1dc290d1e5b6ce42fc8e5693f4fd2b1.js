f04f870530eee5881e36e166c36a9a5452cb5c58
B A-W <lanlecreates@gmail.com>
2019-02-26 22:06:12 +0000

    Updates

---
+++ precache-manifest.a1dc290d1e5b6ce42fc8e5693f4fd2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd3e208b15c36d23552a",
+    "url": "/sales-tally/static/js/runtime~main.fd3e208b.js"
+  },
+  {
+    "revision": "c41c50f031b491ebe248",
+    "url": "/sales-tally/static/js/main.c41c50f0.chunk.js"
+  },
+  {
+    "revision": "7cab43e8349fc111d9df",
+    "url": "/sales-tally/static/js/2.7cab43e8.chunk.js"
+  },
+  {
+    "revision": "c41c50f031b491ebe248",
+    "url": "/sales-tally/static/css/main.e4c853ec.chunk.css"
+  },
+  {
+    "revision": "14e70a29f060a468d346e3781abe8331",
+    "url": "/sales-tally/index.html"
+  }
+];
